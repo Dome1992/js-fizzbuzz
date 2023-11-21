d2c48d52f7bbf66d187b372ccc881a0ee6b4da36
Dome1992 <forlanodomenico92@mail.com>
2023-11-21 15:51:11 +0100

    aggiornato

--- script.js
+++ script.js
@@ -15,15 +15,12 @@ for (let i = 0; i <= 100; i++) {
     if (i % 3 === 0) {
         square += 'Fizz'
     } 
-    else if (i % 5 === 0) {
+     if (i % 5 === 0) {
         square += 'Buzz';
     }
-
-    
-
-
-
-
+    if (i % 3 === 0 && i % 5 === 0) {
+        square += 'FizzBuzz';
+    }
 
 
 }
